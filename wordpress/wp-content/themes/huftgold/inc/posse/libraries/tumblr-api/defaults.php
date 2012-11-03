2d46c88d3fe44979d736436c6e46c31810176db4
Edbury Enegren <edbury.enegren@gmail.com>
2012-11-03 01:08:22 -0400

    add tumblr libraries, for now

---
+++ wordpress/wp-content/themes/huftgold/inc/posse/libraries/tumblr-api/defaults.php
@@ -0,0 +1,11 @@
+<?php
+
+// copy this file to defaults.php and provide the requested values below
+
+define("BASE_HOSTNAME", "edbury.tumblr.com");
+define("API_KEY", "wzhr9lOEWDndxybztr6sTWCtp02a1fJiCp6B3WYaa9Xa2q4Wtz");
+define("API_SECRET", "umXBveWpQnKId3Q7qQTS46UDUQ13IPcmmNhmbvGrsi8t4SclRj");
+define("TOKEN", "sG8SHcbAW8UPFvZkAn6s424WRV19CVMHw46WB02I");
+define("TOKEN_SECRET", "Gqiu2hnbyj4MkA7qD4ffeiKTODak8V0pp7fqvNEvp0");
+
+date_default_timezone_set("America/Los_Angeles");
