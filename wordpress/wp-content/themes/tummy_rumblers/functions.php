d6398c4d749bd4200c4b5d8076e3b97db53c2ad7
Edbury Enegren <edbury.enegren@gmail.com>
2013-05-20 23:48:21 -0400

    post thumbnail support

--- wordpress/wp-content/themes/tummy_rumblers/functions.php
+++ wordpress/wp-content/themes/tummy_rumblers/functions.php
@@ -59,7 +59,7 @@ function tummy_rumblers_setup() {
 	 *
 	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
 	 */
-	//add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'post-thumbnails' );
 
 	/**
 	 * This theme uses wp_nav_menu() in one location.
